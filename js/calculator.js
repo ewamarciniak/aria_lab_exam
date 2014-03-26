4c29f874fd39f9a86dfa8a116182d78f1610ef3a
Ewa Marciniak <emarciniak78@gmail.com>
2014-03-26 19:10:18 +0000

    Created basic calculator Javascript functions - add, subtract, multiply and divide and wrote jasmine tests for them

--- js/calculator.js
+++ js/calculator.js
@@ -1,3 +1,56 @@
 var Calculator = (function() {
 
+    // Private stuff up here
+    var converterValue = 2.2;
+
+    // Public methods here
+    return {
+        addition: function (input_value_1,input_value_2 ) {
+            var num_1 = parseInt(input_value_1);
+            var num_2 = parseInt(input_value_2);
+
+            if (isNaN(num_1) == true || isNaN(num_2) == true) {
+                throw new Error("Not a number");
+            }
+            num = num_1 + num_2;
+            return num;
+        },
+
+        subtraction: function (input_value_1,input_value_2 ) {
+            var num_1 = parseInt(input_value_1);
+            var num_2 = parseInt(input_value_2);
+
+            if (isNaN(num_1) == true || isNaN(num_2) == true) {
+                throw new Error("Not a number");
+            }
+            num = num_1 - num_2;
+            return num;
+        },
+
+        multiplication: function (input_value_1,input_value_2 ) {
+            var num_1 = parseInt(input_value_1);
+            var num_2 = parseInt(input_value_2);
+
+            if (isNaN(num_1) == true || isNaN(num_2) == true) {
+                throw new Error("Not a number");
+            }
+            return num = num_1 * num_2;
+        },
+
+        division: function (input_value_1,input_value_2 ) {
+            var num_1 = parseInt(input_value_1);
+            var num_2 = parseInt(input_value_2);
+
+            if (isNaN(num_1) == true || isNaN(num_2) == true) {
+                throw new Error("Not a number");
+            }
+            if (num_2 == 0){
+                throw new Error("Cannot divide by 0");
+            }
+            num = num_1 / num_2;
+            return Math.round(num);
+        }
+
+    }
+
 }())
--- test/spec/calculatorSpec.js
+++ test/spec/calculatorSpec.js
@@ -1,3 +1,46 @@
 describe ("My Calculator", function() {
+    it("should add two integers", function() {
+        expect (Calculator.addition(33, 20)).toBe(53);
+    })
 
+    it("should be able to deal with strings when adding", function() {
+        expect (function() {
+            Calculator.addition("hello")
+        }).toThrow(new Error("Not a number"));
+    })
+
+    it("should subtract one integer from another", function() {
+        expect (Calculator.subtraction(40,19)).toBe(21);
+    })
+
+    it("should be able to deal with strings when subtracting", function() {
+        expect (function() {
+            Calculator.subtraction("hello")
+        }).toThrow(new Error("Not a number"));
+    })
+
+    it("should multiply one two integers", function() {
+        expect (Calculator.multiplication(20,5)).toBe(100);
+    })
+
+    it("should be able to deal with strings when multiplying", function() {
+        expect (function() {
+            Calculator.multiplication("hello")
+        }).toThrow(new Error("Not a number"));
+    })
+    it("should divide one integer by another", function() {
+        expect (Calculator.division(20,5)).toBe(4);
+    })
+
+    it("should be able to deal with strings when dividing", function() {
+        expect (function() {
+            Calculator.division("hello")
+        }).toThrow(new Error("Not a number"));
+    })
+
+    it("should be able to deal with dividing by 0", function() {
+        expect (function() {
+            Calculator.division(20, 0)
+        }).toThrow(new Error("Cannot divide by 0"));
+    })
 })
